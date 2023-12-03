e2fa479ce821931cf4dc10fe81ba2b22b687a1b9
Amali Delauney <amali.jdelauney@gmail.com>
2023-12-03 13:12:20 -0500

    lvl 7 auto ufo section

---
+++ Assets/DisableOnStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnStart : MonoBehaviour
+{
+    public bool destroy;
+    void Start()
+    {
+        if (!destroy)
+            gameObject.SetActive(false);
+        else
+            Destroy(gameObject);
+    }
+}
